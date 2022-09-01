c47ac22bf60f3f082d349263cce44c8f03d8c9d4
Dzikri52464 <dzikriamrillah86@gmail.com>
2022-09-01 13:32:58 +0700

    update

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //alert ("blajar html");//
-//window.alert("blajar html");//
+window.alert("blajar html");
+
+let input = prompt('berapa umur anda', '')
+alert('apakah umur anda ' + input + " ?")
+console.log(input)
+input = input + " aku teks tambahan"
+console.log(input)
